c91c927e96a7f1dc03002ea1996f0e6c6f4f63ae
Shadee Merhi <shadmerhi@gmail.com>
2022-01-08 16:52:20 -0700

    install router and moment

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,20 +1,31 @@
 import React from "react";
 
+import classNames from "classnames";
+import Moment from 'react-moment';
+
 const LaunchItem = ({ item }) => {
     return (
         <div className="card card-body mb-3">
             <div className="row">
                 <div className="col-md-9">
-                    <h4>{item.mission}</h4>
-                    <p>Date: {item.launch_date_local}</p>
+                    <h4>
+                        <span
+                            className={classNames({
+                                "text-success": item.launch_success,
+                                "text-danger": !item.launch_success,
+                            })}
+                        >
+                            {item.mission_name}
+                        </span>
+                    </h4>
+                    <p>
+                        <Moment format="YYYY-MM-DD HH:mm">{item.launch_date_local}</Moment>
+                    </p>
                 </div>
                 <div className="col-md-3">
-                  <button className="btn btn-primary">
-                    Details
-                  </button>
+                    <button className="btn btn-primary">Details</button>
                 </div>
             </div>
-            {item.mission_name}
         </div>
     );
 };
