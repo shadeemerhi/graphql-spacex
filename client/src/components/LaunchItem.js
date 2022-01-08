fce339819ca14602a37ff1718ae18ea3325074e2
Shadee Merhi <shadmerhi@gmail.com>
2022-01-08 16:43:41 -0700

    add launch item structure

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const LaunchItem = ({ item }) => {
-  return (
-    <div>
-      {item.mission_name}
-    </div>
-  )
-}
+    return (
+        <div className="card card-body mb-3">
+            <div className="row">
+                <div className="col-md-9">
+                    <h4>{item.mission}</h4>
+                    <p>Date: {item.launch_date_local}</p>
+                </div>
+                <div className="col-md-3">
+                  <button className="btn btn-primary">
+                    Details
+                  </button>
+                </div>
+            </div>
+            {item.mission_name}
+        </div>
+    );
+};
 
-export default LaunchItem
+export default LaunchItem;
