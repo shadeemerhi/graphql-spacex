4787dcb2b3235a61c573e3c39a353fc62c54d0a0
Shadee Merhi <shadmerhi@gmail.com>
2022-01-08 16:17:05 -0700

    solve cors, fetch launches success

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -3,22 +3,28 @@ import { gql, useQuery } from "@apollo/client";
 
 const LAUNCHES_QUERY = gql`
     query LaunchesQuery {
-        flight_number
-        mission_name
-        launch_date_local
-        launch_success
+        launches {
+            flight_number
+            mission_name
+            launch_date_local
+            launch_success
+        }
     }
 `;
 
 const Launches = () => {
-    return <div>
-      <h1 className="display-4 my-3">
-        Hello
-      </h1>
-      <button className="btn btn-success">
-        Hello
-      </button>
-    </div>;
+    const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+
+    console.log("HERE IS DATA", loading, error, data);
+
+    if (loading) return <h1 className="display-5 my-3">LOADING...</h1>;
+    if (error) return <h1 className="display-5 my-3">THERE WAS AN ERROR</h1>;
+
+    return (
+        <div>
+            <h1 className="display-5 my-3">Launches</h1>
+        </div>
+    );
 };
 
 export default Launches;
